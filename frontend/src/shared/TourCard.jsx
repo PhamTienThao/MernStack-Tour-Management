e8d7fe4cbd513fd3dd472a2113002f13dead4d2b
BamBoo <phamtienthao88@gmail.com>
2023-02-20 01:44:46 +0700

    Tour List Page, Tour Details, handle submit review

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -24,12 +24,12 @@ const TourCard = ({tour}) => {
               <i class="ri-map-pin-line"></i> {city}
             </span>
             <span className="tour__rating d-flex align-items-center gap-1">
-              <i class="ri-map-pin-line"></i> {avgRating === 0 ? null : avgRating}
+              <i class="ri-star-s-fill"></i> {avgRating === 0 ? null : avgRating}
               {totalRating === 0 ? "Not Rated" : <span>({reviews.length})</span>}
             </span>
           </div>
           <h5 className="tour__title">
-            <Link to={"/tours/${id}"}>{title}</Link>
+            <Link to={`/tours/${id}`}>{title}</Link>
           </h5>
           <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
             <h5>
@@ -37,7 +37,7 @@ const TourCard = ({tour}) => {
               <span> /per persn</span>
             </h5>
             <button className="btn booking__btn">
-              <Link to={"/tours/${id}"}>Book Now</Link>
+              <Link to={`/tours/${id}`}>Book Now</Link>
             </button>
           </div>
         </CardBody>
